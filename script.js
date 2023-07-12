796d391dabeaa24bdc99455f2d71a16f34904a32
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-07-13 02:40:38 +0300

    Adde checkSquare Function and restart button

--- script.js
+++ script.js
@@ -23,4 +23,35 @@ function addGo(e) {
     go = go === "circle" ? "cross" : "circle"
     infoDisplay.textContent = "it is now " + go + "'s Turn."
     e.target.removeEventListener("click", addGo)
+    checkScore()
+}
+
+function checkScore(){
+    let allSquares = document.querySelectorAll(".square")
+    let winningCombos =[
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+    winningCombos.forEach(array =>{
+        let circleWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains('circle'))
+
+        if (circleWins) {
+            infoDisplay.textContent = "Circle Wins!"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
+
+    winningCombos.forEach(array =>{
+        let crossWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains('cross'))
+
+        if (crossWins) {
+            infoDisplay.textContent = "Cross Wins!"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
 }
